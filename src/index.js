a4034afa92690ea57febf7d1fa223312eff900e6
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-10-21 09:35:19 -0600

    small fixes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { fetchWithTimeout } from "./services";
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
-    fetchWithTimeout(1000).then(resolveFunction())
+    return fetchWithTimeout(1000).then(resolveFunction)
 }
 
-var moviePromise = fetchMovies()
-moviePromise.then(result)
-console.log(results)
+const moviePromise = fetchMovies()
+moviePromise.then(results => {
+    console.log(results);
+})
+
 
 var movies = require('./data/movies.json')
