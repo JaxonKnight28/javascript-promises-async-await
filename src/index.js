90242cb512e3823c00a2a031b0fc1dd5f55a0be0
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-10-21 09:32:12 -0600

    more work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export function fetchMovies() {
+import { fetchWithTimeout } from "./services";
 
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000).then(resolveFunction())
 }
+
+var moviePromise = fetchMovies()
+moviePromise.then(result)
+console.log(results)
+
+var movies = require('./data/movies.json')
